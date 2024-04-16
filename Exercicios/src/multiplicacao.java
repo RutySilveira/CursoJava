d9024d6199b761abc03687624a91aa7675a8f5f2
RutySilveira <ruteneles@gmail.com>
2024-04-16 15:38:09 -0300

    Atualizando o arquivo multiplicacao.java

--- Exercicios/src/multiplicacao.java
+++ Exercicios/src/multiplicacao.java
@@ -1,14 +1,19 @@
+// Define uma classe chamada "multiplicacao".
 public class multiplicacao {
 
-  public static void main(String[]args){
+  // Método principal que é executado quando o programa Java é iniciado.
+  public static void main(String[] args) {
+      
+      // Declaração de uma variável inteira 'x' e 'y'.
+      int x, y;
+      // Atribui o valor 5 à variável 'x'.
+      x = 5;
+      // Atribui o dobro do valor de 'x' à variável 'y'.
+      y = 2 * x;
 
-    int x;
-    double y;
-
-    x = 5;
-    y = 2 * x;
-
-    System.out.println(x);
-    System.out.println(y);
+      // Imprime o valor de 'x' no console.
+      System.out.println(x);
+      // Imprime o valor de 'y' no console.
+      System.out.println(y);
   }
 }
