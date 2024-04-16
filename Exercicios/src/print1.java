c1293857c82ffa15de3b7787c45d425574fbed8a
RutySilveira <ruteneles@gmail.com>
2024-04-16 15:42:38 -0300

    Atualizando o arquivo print1.java

--- Exercicios/src/print1.java
+++ Exercicios/src/print1.java
@@ -1,7 +1,12 @@
+// Define uma classe chamada "print1".
 public class print1 {
-  public static void main(String[]args){
-    int y = 32;
+
+  // Método principal que é executado quando o programa Java é iniciado.
+  public static void main(String[] args) {
+      
+      // Declara e inicializa uma variável inteira 'y' com o valor 32.
+      int y = 32;
+      // Imprime o valor da variável 'y' no console.
       System.out.println(y);
   }
 }
-
